e3dd7b6f84becec179429c6ca95c1ba2ab567c35
alexis <alexis_tarrifa@hotmail.com>
2020-09-09 11:44:40 -0500

    version con busqueda cedula y retorno nombre apellido u id

--- indexApiSimple.php
+++ indexApiSimple.php
@@ -10,14 +10,13 @@ if($_SERVER['REQUEST_METHOD']=='GET'){
     if(isset($_GET['usuario_documento'])){
         $query="select `usuario_nombre`,`usuario_apellido`,`usuario_id`,`usuario_documento` from usuarios where usuario_documento=".$_GET['usuario_documento'];
         $resultado=metodoGet($query);
-        $user = 'usuario_id';
+        
         echo json_encode($resultado->fetch(PDO::FETCH_ASSOC));
-        $query="select `meSientoMal`,`meSientoBien`,`date`,`usuario_id` from sintomas where usuario_id = '$user'";
-        $resultado=metodoGet($query);
-        echo json_encode($resultado->fetch(PDO::FETCH_ASSOC));
+        
+        
     }else{
         echo("Cedula no encontrada registrase en el sistema");
-        echo $user;
+       
         // $query="select * from usuarios";
         // $resultado=metodoGet($query);
         // echo json_encode($resultado->fetchAll()); 
