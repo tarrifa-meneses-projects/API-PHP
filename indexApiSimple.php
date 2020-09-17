5a4a32e2a1c3748199692e5eef146d193c382a80
alexis <alexis_tarrifa@hotmail.com>
2020-09-16 20:15:02 -0500

    var

--- indexApiSimple.php
+++ indexApiSimple.php
@@ -13,7 +13,7 @@ if($_SERVER['REQUEST_METHOD']=='GET'){
         
         echo json_encode($resultado->fetch(PDO::FETCH_ASSOC));
         $res = json_encode($resultado->fetch(PDO::FETCH_ASSOC));
-        echo $res;
+        echo "esta variable es".$res;
         
     }else{
         echo("Cedula no encontrada registrase en el sistema");
